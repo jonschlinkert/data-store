e6ac05945dc668876f25b7633c2dda0457b82afb
jonschlinkert <dev@sellside.com>
2014-11-15 10:11:57 -0500

    refactor

--- index.js
+++ index.js
@@ -1,50 +1,203 @@
-var cheerio = require('cheerio');
-var matter = require('gray-matter');
+'use strict';
+
+var path = require('path');
+var typeOf = require('kind-of');
+var fs = require('graceful-fs');
 var _ = require('lodash');
 
-var Store = function (html) {
-  this.html = html;
+/**
+ * Expose `Store`
+ */
+
+module.exports =  Store;
+
+/**
+ * Initialize a new `Store` with the given `name`
+ * and `options`.
+ *
+ * ```js
+ * var store = new Store('bar', 'foo');
+ * //=> saves `{}` to 'foo/.bar.json'
+ *
+ * var store = new Store('baz');
+ * //=> saves `{}` to '~/data-store/.baz.json'
+ * ```
+ *
+ * @param  {String} `name` Dest file name. `foo` would result in `.foo.json`
+ * @param  {String} `dest` Dest directory. If not defined, the user home directory
+ *                         for the current OS is used.
+ * @api public
+ */
+
+function Store(name, dest) {
+  if (typeof name !== 'string') {
+    throw new Error('data-store expects a string as the first argument.');
+  }
+  this.path = path.join((dest || home('data-store')), '.' + name + '.json');
+  this.config = readFile(this.path) || {};
+}
+
+/**
+ * Assign `value` to `key` and save to disk.
+ *
+ * ```js
+ * store.set('foo', 'bar');
+ * // or
+ * store.set({foo: 'bar'});
+ * ```
+ * @param {String} `key`
+ * @param {*} `val` The value to save to `key`. Must be a valid JSON type: String, Number, Array or Object.
+ * @return {Object} `Store` for chaining
+ * @api public
+ */
+
+Store.prototype.set = function(key, val) {
+  if (typeof val === 'function') {
+    throw new Error('Store#set cannot set functions as values:' + String(val));
+  }
+
+  if (typeOf(key) === 'object') {
+    _.extend(this.config, key);
+  } else {
+    this.config[key] = val;
+  }
+
+  this.save();
+  return this;
 };
 
-Store.prototype.set = function(id, context) {
-  if (_.isObject(id)) {
-    context = id;
-    id = undefined;
+/**
+ * Get the stored `value` of `key`, or return all stored values
+ * if no `key` is defined.
+ *
+ * ```js
+ * store.set('foo', 'bar');
+ * store.get('foo');
+ * //=> 'bar'
+ * ```
+ *
+ * @param  {String} `key`
+ * @return {*} The stored value of `key`.
+ * @api public
+ */
+
+Store.prototype.get = function(key) {
+  if (!key) {
+    return _.cloneDeep(this.config);
   }
-  context = context || {};
+  return this.config[key];
+};
 
-  var page = matter(this.html);
-  var _id = id || context.id || 'metadata';
+/**
+ * Save the store to disk.
+ *
+ * ```js
+ * store.save();
+ * ```
+ * @param {String} `dest` Optionally define an alternate destination.
+ * @api public
+ */
 
-  var metadata = _.extend({}, context, page.context);
-  var content = page.content;
-  var $ = cheerio.load(content);
+Store.prototype.save = function(dest) {
+  writeJson(dest || this.path, this.config);
+};
 
-  var script = '<script type="text/x-metadata" id="'+_id+'"></script>';
+/**
+ * Delete a property or array of properties from the store then
+ * re-save the store.
+ *
+ * ```js
+ * store.omit('foo');
+ * // or
+ * store.omit(['foo', 'bar']);
+ * ```
+ *
+ * @param {String|Array} `key` The key(s) to omit from the store
+ * @api public
+ */
 
-  if ($('#' + _id).length === 0) {
-    if ($('body').length) {
-      $('body').append(script);
-      content = $.html();
+Store.prototype.omit = function(keys) {
+  keys = [].concat.apply([], arguments);
+
+  for (var i = 0; i < keys.length; i++) {
+    delete this.config[keys[i]];
+  }
+
+  this.save();
+  return this;
+};
+
+/**
+ * Get the user's home directory
+ *
+ * @api private
+ */
+
+function home(fp) {
+  var res = (process.platform === 'win32')
+    ? process.env.USERPROFILE
+    : process.env.HOME;
+  return path.join(res, fp);
+}
+
+/**
+ * Read JSON files.
+ *
+ * @param {String} `fp`
+ * @return {Object}
+ * @api private
+ */
+
+function readFile(fp) {
+  try {
+    return require(path.resolve(fp));
+  } catch(err) {}
+  return {};
+}
+
+/**
+ * Synchronously write files to disk, also creating any
+ * intermediary directories if they don't exist.
+ *
+ * @param {String} `dest`
+ * @param {String} `str`
+ * @api private
+ */
+
+function writeJson(dest, str, opts) {
+  var dir = path.dirname(dest);
+  if (!fs.existsSync(dir)) {
+    mkdir(dir);
+  }
+
+  try {
+    var json = JSON.stringify(str, null, 2);
+    fs.writeFileSync(dest, json);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+/**
+ * Make the given directory and intermediates
+ * if they don't already exist.
+ *
+ * @param {String} `dirpath`
+ * @param {Number} `mode`
+ * @return {String}
+ * @api private
+ */
+
+function mkdir(dir, mode) {
+  mode = mode || parseInt('0777', 8) & (~process.umask());
+  if (!fs.existsSync(dir)) {
+    var parent = path.dirname(dir);
+
+    if (fs.existsSync(parent)) {
+      fs.mkdirSync(dir, mode);
     } else {
-      content += script;
+      mkdir(parent);
+      fs.mkdirSync(dir, mode);
     }
   }
-
-  $ = cheerio.load(content);
-  $('#' + _id).attr('data-metadata', JSON.stringify(metadata));
-
-  this.html = $.html();
-};
-
-
-Store.prototype.get = function(id) {
-  var _id = id || 'metadata';
-  var results = cheerio.load(this.html)('#' + _id).data('metadata');
-  return results;
-};
-
-
-module.exports = function(html) {
-  return new Store(html);
-};
+}
