ad4be8a76aefa5364ad60a7f61420bc857fedb4c
jonschlinkert <jon.schlinkert@sellside.com>
2015-03-27 23:31:01 -0400

    update example

--- example.js
+++ example.js
@@ -1,14 +1,17 @@
-'use strict';
-
 var Store = require('./');
-var store = new Store('app');
+var store = new Store('app', {cwd: 'actual'});
 
 store
   .set('a', 'b')
   .set('c', 'd')
   .set('e', 'f');
 
-store.save('actual/app.json');
+store.save();
+
+console.log(store.get());
+store.omit('a');
 
 console.log(store.get());
 store.delete();
+
+console.log(store.get());
