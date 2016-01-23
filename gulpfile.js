83e516bc4e99d44ed898d06730cdfccf4c1205cb
jonschlinkert <jon.schlinkert@sellside.com>
2016-01-23 04:12:55 -0500

    lint

--- gulpfile.js
+++ gulpfile.js
@@ -7,19 +7,19 @@ var eslint = require('gulp-eslint');
 
 var lint = ['index.js', 'utils.js'];
 
-gulp.task('coverage', function () {
+gulp.task('coverage', function() {
   return gulp.src(lint)
     .pipe(istanbul())
     .pipe(istanbul.hookRequire());
 });
 
-gulp.task('mocha', ['coverage'], function () {
+gulp.task('mocha', ['coverage'], function() {
   return gulp.src('test.js')
     .pipe(mocha({reporter: 'spec'}))
     .pipe(istanbul.writeReports());
 });
 
-gulp.task('eslint', function () {
+gulp.task('eslint', function() {
   return gulp.src(lint.concat('test.js'))
     .pipe(eslint())
 });
